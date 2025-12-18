4f6c99c230f85a2a65554c1af484e9baa826d7c9
bala-amypo <bala@amypo.in>
2025-12-18 07:58:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/validationentity.java
+++ src/main/java/com/example/demo/entity/validationentity.java
@@ -1,7 +1,16 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 @Entity
 public class validationentity{
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
 private Long id;
-private Long username
+@Notnull
+private String username;
+private String email;
+private String password;
+private int age;
 }
