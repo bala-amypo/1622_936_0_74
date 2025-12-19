6243f6cd45798816035759929d9abcb2a20a01a3
bala-amypo <bala@amypo.in>
2025-12-19 09:56:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/idcardentity.java
+++ src/main/java/com/example/demo/entity/idcardentity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
 @Entity
 @Data
 @AllArgsConstructor
@@ -16,5 +18,6 @@ public class idcardentity{
     private Integer id;
     private int cardno;
     @OneToOne
+    @JoinColumn(name = "id")
     private studentdetentity details;
 }
