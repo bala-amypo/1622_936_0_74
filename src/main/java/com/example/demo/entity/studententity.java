bdd82ff155059017aeab9924c31dbbffb280e623
bala-amypo <bala@amypo.in>
2025-12-19 06:29:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/studententity.java
+++ src/main/java/com/example/demo/entity/studententity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.util.Date;
+
 @Entity
+@Data
 public class studententity{
  @Id
  @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -13,44 +15,44 @@ public class studententity{
  private String password;
  private String email;
  private Date created;
- public void setId(Integer id){
-    this.id=id;
- }
- public void setUsername(String username){
-    this.username=username;
- }
- public void setEmail(String email){
-    this.email=email;
- }
- public void setPassword(String password){
-    this.password=password;
- }
- public void setCreated(Date created){
-    this.created=created;
- }
- public Integer getId(){
-    return id;
- }
- public String getUsername(){
-    return username;
- }
- public String getEmail(){
-    return email;
- }
- public String getPassword(){
-    return password;
- }
- public Date getCreated(){
-    return created;
- }
- public studententity(){
+//  public void setId(Integer id){
+//     this.id=id;
+//  }
+//  public void setUsername(String username){
+//     this.username=username;
+//  }
+//  public void setEmail(String email){
+//     this.email=email;
+//  }
+//  public void setPassword(String password){
+//     this.password=password;
+//  }
+//  public void setCreated(Date created){
+//     this.created=created;
+//  }
+//  public Integer getId(){
+//     return id;
+//  }
+//  public String getUsername(){
+//     return username;
+//  }
+//  public String getEmail(){
+//     return email;
+//  }
+//  public String getPassword(){
+//     return password;
+//  }
+//  public Date getCreated(){
+//     return created;
+//  }
+//  public studententity(){
 
- }
- public studententity(Integer id,String username,String email,String password,Date created){
-    this.id=id;
-    this.username=username;
-    this.email=email;
-    this.password=password;
-    this.created=created;
- }
+//  }
+//  public studententity(Integer id,String username,String email,String password,Date created){
+//     this.id=id;
+//     this.username=username;
+//     this.email=email;
+//     this.password=password;
+//     this.created=created;
+//  }
 }
--- src/main/java/com/example/demo/entity/validationentity.java
+++ src/main/java/com/example/demo/entity/validationentity.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Email;
 @Entity
+
 public class validationentity{
  @Id
  @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -24,50 +25,50 @@ private String password;
  @Max(30)
  @Positive(message = "Age must be a positive number")
 private int age;
- public void setId(Long id){
-    this.id=id;
- }
- public void setUsername(String username){
-    this.username=username;
- }
- public void setEmail(String email){
-    this.email=email;
- }
- public void setPassword(String password){
-    this.password=password;
- }
- public void setAge(int age){
-    this.age=age;
- }
- public Long getId(){
-    return id;
- }
- public String getUsername(){
-    return username;
- }
- public String getEmail(){
-    return email;
- }
- public String getPassword(){
-    return password;
- }
- public int getAge(){
-    return age;
- }
- public validationentity(){
+//  public void setId(Long id){
+//     this.id=id;
+//  }
+//  public void setUsername(String username){
+//     this.username=username;
+//  }
+//  public void setEmail(String email){
+//     this.email=email;
+//  }
+//  public void setPassword(String password){
+//     this.password=password;
+//  }
+//  public void setAge(int age){
+//     this.age=age;
+//  }
+//  public Long getId(){
+//     return id;
+//  }
+//  public String getUsername(){
+//     return username;
+//  }
+//  public String getEmail(){
+//     return email;
+//  }
+//  public String getPassword(){
+//     return password;
+//  }
+//  public int getAge(){
+//     return age;
+//  }
+//  public validationentity(){
 
- }
- public validationentity(Long id,
-       @NotNull
-       @Size(min = 1,max = 6,message = "must be 2 to 6 characters")String username,
-       @Email(message= "Email is not valid")String email,
-       @Size(min = 2,max = 6,message ="Password must be 2 to 6 characters")
-       @NotNull(message = "Password is mandatory")String password,
-       @Max(30) @Positive(message = "Age must be a positive number")int age){
-    this.id=id;
-    this.username=username;
-    this.email=email;
-    this.password=password;
-    this.age=age;
- }
+//  }
+//  public validationentity(Long id,
+//        @NotNull
+//        @Size(min = 1,max = 6,message = "must be 2 to 6 characters")String username,
+//        @Email(message= "Email is not valid")String email,
+//        @Size(min = 2,max = 6,message ="Password must be 2 to 6 characters")
+//        @NotNull(message = "Password is mandatory")String password,
+//        @Max(30) @Positive(message = "Age must be a positive number")int age){
+//     this.id=id;
+//     this.username=username;
+//     this.email=email;
+//     this.password=password;
+//     this.age=age;
+//  }
 }
